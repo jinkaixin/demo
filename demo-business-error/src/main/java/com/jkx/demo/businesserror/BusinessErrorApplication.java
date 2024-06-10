d7174435d5e797d07391882c792a9828b57dee68
jkx <2729426040@qq.com>
2024-06-10 09:48:56 +0800

    业务异常-模块初始化

---
+++ demo-business-error/src/main/java/com/jkx/demo/businesserror/BusinessErrorApplication.java
@@ -0,0 +1,13 @@
+package com.jkx.demo.businesserror;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BusinessErrorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BusinessErrorApplication.class, args);
+	}
+
+}
---
+++ demo-business-error/src/test/java/com/jkx/demo/businesserror/BusinessErrorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jkx.demo.businesserror;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BusinessErrorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
